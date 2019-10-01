024a7c89e901bb83935df378962aaa6e0943c3d3
felipunky <feli20gutierrez@hotmail.com>
2019-10-01 03:28:02 -0500

    Add image transition
    image

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 $( document ).ready( function()
 {
+                    
     $( ".Blur" ).on( "mouseenter touchstart", function()
     {
                     
-            $( ".Title" ).css( "color", "#ffbf00" );
-            $( ".TitleColour" ).css( "color", "#ffffff" );
+        $( ".Title" ).css( "color", "#ffbf00" );
+        $( ".TitleColour" ).css( "color", "#ffffff" );
         
     });
                     
@@ -14,6 +15,34 @@ $( document ).ready( function()
         $( ".Title" ).css( "color", "#ffffff" );
         $( ".TitleColour" ).css( "color", "#ffbf00" );
         
+    });
+            
+                    
+    $( ".carousel-item" ).on( "mouseenter touchstart", function()
+    {
+         
+         $( ".carousel-inner img" ).animate( { opacity: '+=0.1' }, { step: function( now, fx )
+                                            { $( this ).css( { 'transform':'scale( '+now+' )' } ); }, duration : 1000 }, 'linear' );
+        //$( ".carousel-inner img" ).animate( { 'transform':'scale( 1.5 )' }, 1000 );
+    
+    });
+                
+    /*$('#foo').animate({  borderSpacing: -90 }, {
+                      step: function(now,fx) {
+                      $(this).css('-webkit-transform','rotate('+now+'deg)');
+                      $(this).css('-moz-transform','rotate('+now+'deg)');
+                      $(this).css('transform','rotate('+now+'deg)');
+                      },
+                      duration:'slow'
+                      },'linear');*/
+    
+    $( ".carousel-item" ).on( "mouseleave touchend", function()
+    {
+    
+        //$( ".carousel-inner img" ).css( { 'transform':'scale( 1.0 )' } );
+        $( ".carousel-inner img" ).animate( { opacity: '-=0.1' }, { step: function( now, fx )
+                                            { $( this ).css( { 'transform':'scale( '+now+' )' } ); }, duration : 1000 }, 'linear' );
+                             
     });
                     
 });
