9d83a3387f3873790df5eab77310c99a082af299
felipunky <feli20gutierrez@hotmail.com>
2019-10-01 23:48:39 -0500

    Smoother transitions
    Update

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
 $( document ).ready( function()
 {
+               
+    $( ".carousel-caption" ).on( "mouseenter touchstart", function()
+    {
+    
+        $( ".Title" ).fadeOut( 500 );
+                                
+    
+    });
                     
-    $( ".carousel-inner img" ).prop( "st", 1 );
+    $( ".carousel-caption" ).on( "mouseleave touchend", function()
+    {
+    
+        $( ".Title" ).fadeIn( 500 );
+                                
+    });
                     
     $( ".Blur" ).on( "mouseenter touchstart", function()
     {
@@ -10,6 +23,7 @@ $( document ).ready( function()
         $( ".TitleColour" ).css( "color", "#ffffff" );
                     
         $( ".carousel-inner img" ).css( "filter", "blur(4px)" );
+        $( ".HTMLLink" ).fadeOut( 500 );
         /*$( ".carousel-inner img" ).animate( { st: '+=0.1' }, { step: function( st, fx )
                                             { $( this ).css( { 'transform':'scale( '+st+' )' } ); }, duration : '1000' }, 'linear' );*/
         
@@ -21,26 +35,33 @@ $( document ).ready( function()
         $( ".Title" ).css( "color", "#ffffff" );
         $( ".TitleColour" ).css( "color", "#ffbf00" );
         $( ".carousel-inner img" ).css( "filter", "blur(0px)" );
+        $( ".HTMLLink" ).fadeIn( 500 );
                     //$( ".carousel-inner img" ).css( "filter", "blur(0px)" );
         /*$( ".carousel-inner img" ).animate( { st: '1.0' }, { step: function( st, fx )
                                             { $( this ).css( { 'transform':'scale( '+st+' )' } ); }, duration : '1000' }, 'linear' );*/
         
     });
                     
-    $( ".carousel-inner img" ).on( "mouseenter touchstart", function()
+    $( ".carousel-inner img" ).prop( "st", 1 );
+                    
+    var counter = 0.0;
+                    
+    $( ".carousel-inner img:not(.carousel-caption):not(.carousel-indicators):not(.carousel-control-next):not(.carousel-control-prev)" ).on( "mouseenter touchstart", function()
     {
-                                  
-        $( this ).animate( { st: '+=0.1' }, { step: function( st, fx )
-                           { $( this ).css( { 'transform':'scale( '+st+' )' } ); }, duration : '800' }, 'linear' );
-    
+           
+                                                                                                                                    
+        $( this ).animate( { st: '+=0.04' }, { step: function( ux, fx )
+                           { $( this ).css( { 'transform':'scale( '+ux+' )' } ); }, duration : '500' }, 'linear' );
+
+                                                                                                                                           
     });
                 
-    $( ".carousel-inner img" ).on( "mouseleave touchend", function()
+    $( ".carousel-inner img:not(.carousel-caption):not(.carousel-indicators):not(.carousel-control-next):not(.carousel-control-prev)" ).on( "mouseleave touchend", function()
     {
-    
-        $( this ).animate( { st: '1.0' }, { step: function( st, fx )
-                           { $( this ).css( { 'transform':'scale( '+st+' )' } ); }, duration : '800' }, 'linear' );
+
+        $( this ).animate( { st: '1.0' }, { step: function( ux, fx )
+                           { $( this ).css( { 'transform':'scale( '+ux+' )' } ); }, duration : '500' }, 'linear' );
                              
     });
-                    
+    
 });
